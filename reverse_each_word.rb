cc1036c11f549c5488315dd067f700e7c4a45cc5
Preston Elliott <preston.elliott24@gmail.com>
2019-03-03 23:08:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,13 @@
-def reverse_each_word 
-  
+def reverse_each_word
+  word.collect do |word|
+    
+  end
+end
+
+
+# .collect example
+# def change_nums(nums)
+#   nums.collect do |x| 
+#     x + 1
+#   end
+# end
