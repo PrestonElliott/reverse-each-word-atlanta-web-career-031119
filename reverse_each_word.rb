1208cf84398ab8dce4a2a30f63c6a8da8a03d6f8
Preston Elliott <preston.elliott24@gmail.com>
2019-03-03 23:57:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
-  string.split(" ") 
-  return string.each {|word| puts word.reverse}
+  puts string.split(" ") 
+  string.each {|word| puts word.reverse}
 end
 
 # one\ntwo\nthree".each {|word| puts word.capitalize}
